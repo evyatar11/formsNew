b1a945f65c2978cee0096edd7a5db02dea5a6e78
Evyatar Atias <evia.at@gmail.com>
2018-05-31 20:45:34 +0300

    graph and table addition

--- src/main/webapp/js/graph.js
+++ src/main/webapp/js/graph.js
@@ -43,7 +43,7 @@ $(document).ready(function(){
         [ "Martena Mccray", "Post-Sales support", "Edinburgh", "8240", "2011/03/09", "$324,050" ],
         [ "Unity Butler", "Marketing Designer", "San Francisco", "5384", "2009/12/09", "$85,675" ]
     ];
-    $('#container').DataTable( {
+    $('#table').DataTable( {
         data: dataSet,
         columns: [
             { title: "Number" },
@@ -52,20 +52,7 @@ $(document).ready(function(){
             { title: "Score Contribution" }
         ]
     } );
-
-    window.chartColors = {
-        red: 'rgb(255, 99, 132)',
-        orange: 'rgb(255, 159, 64)',
-        yellow: 'rgb(255, 205, 86)',
-        green: 'rgb(75, 192, 192)',
-        blue: 'rgb(54, 162, 235)',
-        purple: 'rgb(153, 102, 255)',
-        grey: 'rgb(201, 203, 207)'
-    };
-
     var ctx = document.getElementById('myChart').getContext('2d');
-    var backgroundColors = [];
-    var borderColors = [];
     var data = [
         // {x:0,y:0},{x:1,y:0},{x:2,y:0},{x:3,y:0},{x:4,y:0},{x:5,y:0},{x:6,y:0},{x:7,y:0},{x:8,y:0},{x:9,y:0},{x:10,y:0},
         {x:1,y:12.33},{x:1,y:6.789},{x:3,y:8.96},{x:2,y:2.33},{x:4,y:6.789},{x:5,y:8.96},{x:6,y:2.33},{x:7,y:6.789},{x:8,y:8.96},{x:9,y:16.789},
@@ -75,19 +62,6 @@ $(document).ready(function(){
     var xyisPoints = [
         {x:0,y:0},{x:1,y:0},{x:2,y:0},{x:3,y:0},{x:4,y:0},{x:5,y:0},{x:6,y:0},{x:7,y:0},{x:8,y:0},{x:9,y:0},{x:10,y:0}
         ];
-    for (i=0;i<data.length;i++){
-        if (i==3){
-            //Red color and border - the newly filled form
-            backgroundColors.push('rgba(255, 99, 132, 0.2)');
-            borderColors.push('rgba(255,99,132,1)');
-        }
-        else{
-            //Green color and border - the rest
-            backgroundColors.push('rgba(75, 192, 192, 0.2)');
-            borderColors.push('rgba(75, 192, 192, 1)');
-        }
-    }
-    var borrowerRating = 14;
     var scatterChart = new Chart(ctx, {
         type: 'scatter',
         data: {
