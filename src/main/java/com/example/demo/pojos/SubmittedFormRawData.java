285788df5f228ade296020e7a8efb1c0acc644bb
C5258898 <evyatar.atias@sap.com>
2018-07-19 09:14:05 +0300

    Remover unused files, Added requested details to Submitted form json file, downloaded all needed remote scripts localy (Except one css file we images referances)

--- src/main/java/com/example/demo/pojos/SubmittedFormRawData.java
+++ src/main/java/com/example/demo/pojos/SubmittedFormRawData.java
@@ -7,6 +7,18 @@ import com.google.gson.annotations.SerializedName;
 
 public class SubmittedFormRawData {
 
+    @SerializedName("accountOfficeName")
+    @Expose
+    private String accountOfficeName;
+    @SerializedName("borrowerRating")
+    @Expose
+    private String borrowerRating;
+    @SerializedName("customerName")
+    @Expose
+    private String customerName;
+    @SerializedName("pdScore")
+    @Expose
+    private String pdScore;
     @SerializedName("qa")
     @Expose
     private List<Qa> qa = null;
@@ -27,6 +39,14 @@ public class SubmittedFormRawData {
         this.qa = qa;
     }
 
+    public SubmittedFormRawData(String accountOfficeName, String borrowerRating, String customerName, String pdScore, List<Qa> qa) {
+        this.accountOfficeName = accountOfficeName;
+        this.borrowerRating = borrowerRating;
+        this.customerName = customerName;
+        this.pdScore = pdScore;
+        this.qa = qa;
+    }
+
     public List<Qa> getQa() {
         return qa;
     }
@@ -35,4 +55,35 @@ public class SubmittedFormRawData {
         this.qa = qa;
     }
 
+    public String getAccountOfficeName() {
+        return accountOfficeName;
+    }
+
+    public void setAccountOfficeName(String accountOfficeName) {
+        this.accountOfficeName = accountOfficeName;
+    }
+
+    public String getBorrowerRating() {
+        return borrowerRating;
+    }
+
+    public void setBorrowerRating(String borrowerRating) {
+        this.borrowerRating = borrowerRating;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getPdScore() {
+        return pdScore;
+    }
+
+    public void setPdScore(String pdScore) {
+        this.pdScore = pdScore;
+    }
 }
